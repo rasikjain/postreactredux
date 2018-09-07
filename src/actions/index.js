055f41ae1deb3f47323146215d97479fe39bbadb
“Rasik <jainrasik@gmail.com>
2018-09-07 10:09:38 +0530

    creating form

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import uuid from "uuid/v4";
+import { ADD_POST, DELETE_POST } from "./types";
+
+export const  createPost = ({title, body}) = ({
+
+    type: ADD_POST,
+    payload: {
+        id: uuidv4(),
+        title,
+        body
+    }
+});
+
+export const deletePost = (id) => ({
+    type: DELETE_POST,
+    payload: {
+        id
+    }
+})
